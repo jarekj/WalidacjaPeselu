5a9a67364f76b6b936b0ef9f2d05de092e1666fd
Jarek <jjankowski1@gmail.com>
2018-06-06 19:59:58 +0200

    poprawki

--- src/Main.java
+++ src/Main.java
@@ -6,10 +6,12 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Podaj sprawdzany PESEL: ");
         String pesel = scanner.nextLine();
-        System.out.println("Sprawdzany PESEL: " + pesel);
-        System.out.println(Pesel.isPeselOK(pesel));
-        System.out.println(Pesel.dateOfBirth(pesel));
-        System.out.println(Pesel.sex(pesel));
-
+        if (Pesel.isPeselOK(pesel)) {
+            System.out.println(Pesel.isPeselOK(pesel));
+            System.out.println(Pesel.dateOfBirth(pesel));
+            System.out.println(Pesel.sex(pesel));
+        } else {
+            System.out.println("Nieprawidłowy PESEL.");
+        }
     }
 }
--- src/Pesel.java
+++ src/Pesel.java
@@ -1,50 +1,57 @@
 public class Pesel {
     public static boolean isLengthOK(String pesel) {
-        if (pesel.length() != 11) return false;
-        else return true;
+        return pesel.length() == 11;
     }
 
-    public static String isPeselOK(String pesel) {
+    public static boolean isPeselOK(String pesel) {
         if (isLengthOK(pesel)) {
             byte[] PESEL = new byte[11];
+            byte[] peselWeight = {1, 3, 7, 9, 1, 3, 7, 9, 1, 3};
             int peselLength = pesel.length();
             for (int i = 0; i < peselLength; i++) {
                 PESEL[i] = Byte.parseByte(pesel.substring(i, i + 1));
             }
-                int sum = 1 * PESEL[0] +
-                        3 * PESEL[1] +
-                        7 * PESEL[2] +
-                        9 * PESEL[3] +
-                        1 * PESEL[4] +
-                        3 * PESEL[5] +
-                        7 * PESEL[6] +
-                        9 * PESEL[7] +
-                        1 * PESEL[8] +
-                        3 * PESEL[9];
-                sum = sum % 10;
-                sum = 10 - sum;
-            if(sum == PESEL[10]){
-                return  "Pesel prawidłowy";
-            };
-            return "Nieprawidłowy pesel";
-        } else return "Nieprawidłowy pesel";
+            int sum = peselWeight[0] * PESEL[0] +
+                    peselWeight[1] * PESEL[1] +
+                    peselWeight[2] * PESEL[2] +
+                    peselWeight[3] * PESEL[3] +
+                    peselWeight[4] * PESEL[4] +
+                    peselWeight[5] * PESEL[5] +
+                    peselWeight[6] * PESEL[6] +
+                    peselWeight[7] * PESEL[7] +
+                    peselWeight[8] * PESEL[8] +
+                    peselWeight[9] * PESEL[9];
+            sum = sum % 10;
+            sum = 10 - sum;
+            if (sum == PESEL[10]) {
+                return true;
+            }
+            return false;
+        } else return false;
     }
 
 
     public static String dateOfBirth(String pesel) {
-        String year = pesel.substring(0, 2);
-        year = "19" + year;
-        String month = pesel.substring(2, 4);
-        String day = pesel.substring(4, 6);
-        return "Data urodzania to: rok " + year + ", miesiac " + month + ", dzień " + day + ".";
+        if (isPeselOK(pesel)) {
+
+            String year = pesel.substring(0, 2);
+            year = "19" + year;
+            String month = pesel.substring(2, 4);
+            String day = pesel.substring(4, 6);
+            return "Data urodzania to: rok " + year + ", miesiac " + month + ", dzień " + day + ".";
+        }
+        return "PESEL nieprawidłowy";
     }
 
     public static String sex(String pesel) {
-        int sex = Integer.parseInt(pesel.substring(9, 10));
-        if (sex % 2 == 0) {
-            return "Płeć: kobieta";
-        } else {
-            return "Płeć: mężczyzna";
+        if (isPeselOK(pesel)) {
+            int sex = Integer.parseInt(pesel.substring(9, 10));
+            if (sex % 2 == 0) {
+                return "Płeć: kobieta";
+            } else {
+                return "Płeć: mężczyzna";
+            }
         }
+        return "PESEL nieprawidłowy";
     }
 }
